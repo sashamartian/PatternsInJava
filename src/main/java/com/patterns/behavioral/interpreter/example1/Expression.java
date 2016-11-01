559dd315bb97957f06648b00caf8fec46ad95819
sashamartian <sasha.martian@yandex.ru>
2016-11-01 17:00:36 +0700

    Паттерн Interpreter

---
+++ src/main/java/com/patterns/behavioral/interpreter/example1/Expression.java
@@ -0,0 +1,5 @@
+package main.java.com.patterns.behavioral.interpreter.example1;
+
+public interface Expression {
+    public boolean interpret(String context);
+}
---
+++ src/main/java/com/patterns/behavioral/interpreter/example1/TerminalExpression.java
@@ -0,0 +1,19 @@
+package main.java.com.patterns.behavioral.interpreter.example1;
+
+public class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data){
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+
+        if(context.contains(data)){
+            return true;
+        }
+        return false;
+    }
+}
